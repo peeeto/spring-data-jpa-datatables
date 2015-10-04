73f3da8d71d2c05967bb42294bf5f77129040f6f
Peter Chocholacek <petercho@Peters-MacBook-Pro.local>
2015-10-04 23:39:22 +0300

    multiproperty paging added, java 1.8 as default

--- src/main/java/org/springframework/data/jpa/datatables/repository/DataTablesRepositoryImpl.java
+++ src/main/java/org/springframework/data/jpa/datatables/repository/DataTablesRepositoryImpl.java
@@ -1,13 +1,9 @@
 package org.springframework.data.jpa.datatables.repository;
 
-import java.io.Serializable;
-
-import javax.persistence.EntityManager;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 import org.springframework.data.jpa.datatables.parameter.ColumnParameter;
@@ -17,9 +13,14 @@ import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Repository implementation
- * 
+ *
  * @author Damien Arrachequesne
  */
 public class DataTablesRepositoryImpl<T, ID extends Serializable> extends
@@ -62,25 +63,30 @@ public class DataTablesRepositoryImpl<T, ID extends Serializable> extends
 	/**
 	 * Creates a 'LIMIT .. OFFSET .. ORDER BY ..' clause for the given
 	 * {@link DataTablesInput}.
-	 * 
+	 *
 	 * @param input
 	 *            the {@link DataTablesInput} mapped from the Ajax request
 	 * @return a {@link Pageable}, must not be {@literal null}.
 	 */
-	private Pageable getPageable(DataTablesInput input) {
-		OrderParameter order = input.getOrder().get(0);
-		ColumnParameter column = input.getColumns().get(order.getColumn());
+    private Pageable getPageable(DataTablesInput input) {
+        List<Sort.Order> springOrders = new ArrayList<>();
+        List<OrderParameter> orders = input.getOrder();
+        for (OrderParameter order : orders) {
+            ColumnParameter column = input.getColumns().get(order.getColumn());
+            if (column.getOrderable()) {
+                String sortColumn = column.getData();
+                Sort.Direction sortDirection = Sort.Direction.fromString(order.getDir());
+                Sort.Order springOrder = new Sort.Order(sortDirection, sortColumn);
+                springOrders.add(springOrder);
+            }
+        }
+        final Sort springSort;
+        if (!springOrders.isEmpty()) {
+            springSort = new Sort(springOrders);
+        } else {
+            springSort = null;
+        }
 
-		if (column.getOrderable()) {
-			String sortColumn = column.getData();
-			Direction sortDirection = Direction.fromString(order.getDir());
-
-			return new PageRequest(input.getStart() / input.getLength(),
-					input.getLength(), sortDirection, sortColumn);
-		} else {
-			return new PageRequest(input.getStart() / input.getLength(),
-					input.getLength());
-		}
-	}
-
-}
+        return new PageRequest(input.getStart() / input.getLength(), input.getLength(), springSort);
+    }
+  }
